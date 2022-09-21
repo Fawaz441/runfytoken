2660f785bb9b415e31ccdad249223f7b8a371d41
Fawaz441 <abdulsalamfawaz4@gmail.com>
2022-09-21 06:36:25 +0100

    fixed z-index

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -39,7 +39,7 @@ gsap.to(".hero-man", {
   duration: 1,
   y: -40,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
     toggleActions: "play reverse reverse reverse",
   },
 });
@@ -48,7 +48,7 @@ gsap.to(".hero-bg", {
   duration: 1,
   y: -300,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
     toggleActions: "play reverse reverse reverse",
   },
 });
@@ -57,7 +57,7 @@ gsap.to(".hero-rocks", {
   duration: 1,
   y: -60,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
     toggleActions: "play reverse reverse reverse",
   },
 });
@@ -66,7 +66,7 @@ gsap.to(".hero-coins", {
   duration: 1,
   x: -100,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
     toggleActions: "play reverse reverse reverse",
   },
 });
@@ -75,7 +75,7 @@ gsap.to(".hero-water", {
   duration: 1,
   x: -50,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
     toggleActions: "play reverse reverse reverse",
   },
 });
@@ -86,7 +86,7 @@ gsap.to(".runfy-description-1", {
   text: `Runfy is a 100% community-driven platform that empowers members to
   take control of their health and wellness.`,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
   },
 });
 
@@ -98,7 +98,7 @@ gsap.to(".runfy-description-2", {
   the platform built on the BSC (Binance Smart Chain) which has one of the lowest
   transaction fees.`,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
   },
 });
 
@@ -110,7 +110,7 @@ gsap.to(".runfy-description-3", {
   money while keeping fit. Whatever your fitness goals may be, the Runfy team is here to
   assist you.`,
   scrollTrigger: {
-    trigger: "#what",
+    trigger: ".bonuscontainer",
   },
 });
 
